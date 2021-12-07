1a0d3c070de55be2313b5a24c54eb908c164237f
Your Name <oliver.pulsford@anu.edu.au>
2021-12-08 10:28:26 +1100

    beginning automated play work

--- minesweeper.c
+++ minesweeper.c
@@ -11,6 +11,15 @@
        __typeof__ (b) _b = (b); \
      _a < _b ? _a : _b; })
 
+// Move data structure
+struct Move {
+    // Row and column of target tile
+    int row;
+    int col;
+    // Type of move -> 0 means reveal, 1 means flag
+    int type;
+};
+
 void board_init(int* board, int mines, int total_squares, int board_height, int board_width) {
     // Seed random number gen
     srand(0);
@@ -115,6 +124,75 @@ void print_actual_board(int *board, int board_height, int board_width) {
     printf("|\n\n");
 }
 
+void take_move_input(struct Move *move) {
+    int r, c, t;
+    scanf("%i %i %i", &r, &c, &t);
+    move->row = r-1;
+    move->col = c-1;
+    move ->type = t;
+}
+
+void calculate_move(struct Move *move, double *probability_board) {
+    
+}
+
+int apply_move(struct Move *move, int *board, char *disp_board, int board_height, int board_width, int *flags_remaining) {
+    // Check move is valid
+    if (move->row < 0 || move->col < 0 || move->row > board_height-1 || move->col > board_width-1 || move->type > 2 || move->type < 0) {
+        printf("Move is invalid. Please use only valid row and column indices and a 0, 1 or 2 to indicate reveals and flags adds/removes.\n");
+    }
+
+    int row = move->row;
+    int col = move->col;
+
+    int tile = board[row*board_width+col];
+
+    // If revealing a tile
+    if (move->type == 0 && disp_board[row*board_width+col] == '-') {
+        // Check what tile is
+        if (tile == -1) {
+            return -1;
+        } else if (tile == 0) {
+            disp_board[row*board_width+col] = ' ';
+
+            // Reveal all adjacent tiles.
+            for (int i = max(0, row - 1); i < min(board_height, row + 2); i++) {
+                for (int j = max(0, col - 1); j < min(board_width, col + 2); j++) {
+                    // printf("%i %i\n", i, j);
+                    if (!(i == row && j == col)) {
+                        struct Move rev_tile;
+                        rev_tile.row = i;
+                        rev_tile.col = j;
+                        rev_tile.type = 0;
+                        apply_move(&rev_tile, board, disp_board, board_height, board_width, flags_remaining);
+                    }
+                }
+            }
+        } else {
+            // Reveal Tile
+            disp_board[row*board_width+col] = tile + '0';
+        }
+    } else if (move->type == 1 && disp_board[row*board_width+col] == '-'){
+        // Flag the tile
+        disp_board[row*board_width+col] = 'P';
+        *flags_remaining -= 1;
+    } else if (move->type == 2 && disp_board[row*board_width+col] == 'P'){
+        // Flag the tile
+        disp_board[row*board_width+col] = '-';
+        *flags_remaining += 1;
+    }
+
+    // Check completion status
+    int complete = 1;
+
+    for (int i = 0; i < board_height*board_width; i++) {
+        complete = complete && (disp_board[i] != '-');
+    }
+
+    complete = complete && *flags_remaining == 0;
+
+    return complete;
+}
 
 
 int main(int argc, char *argv[]) {
@@ -124,6 +202,14 @@ int main(int argc, char *argv[]) {
     int board_height = atoi(argv[2]);
     // Third is difficulty
     int difficulty = atoi(argv[3]);
+    // Fourth is User play mode
+    int play_mode = atoi(argv[4]);
+
+    // Game Status Variable. 0 = In progress, -1 = Lost, 1 = Won.
+    int game_status = 0;
+
+    // Move Data structure
+    struct Move curr_move;
     
     // Create board data structures
     int total_squares = board_height*board_width;
@@ -142,13 +228,13 @@ int main(int argc, char *argv[]) {
                 mines = total_squares*0.1;
                 break;
             case 1:
-                mines = total_squares*0.25;
+                mines = total_squares*0.3;
                 break;
             case 2:
-                mines = total_squares*0.5;
+                mines = total_squares*0.6;
                 break;
             case 3:
-                mines = total_squares*0.7;
+                mines = total_squares*0.8;
                 break;
             default: 
                 mines = total_squares;
@@ -159,6 +245,27 @@ int main(int argc, char *argv[]) {
     board_init(board, mines, total_squares, board_height, board_width);
     other_boards_init(display_board, probability_board, total_squares);
     print_display_board(display_board, board_height, board_width);
+    // print_actual_board(board, board_height, board_width);
+    
+    while (game_status == 0) {
+        if (play_mode == 0) {
+            take_move_input(&curr_move);
+        } else {
+            calculate_move(&curr_move, probability_board);
+        }
+        
+        game_status = apply_move(&curr_move, board, display_board, board_height, board_width, &mines);
+        print_display_board(display_board, board_height, board_width);
+        // print_actual_board(board, board_height, board_width);
+    }
+
+    // Completion Message
+    if (game_status == 1) {
+        printf("\n\nCONGRATULATIONS YOU JUST WON!!\n\n");
+    } else {
+        printf("\n\nOUCH! YOU JUST STEPPED ON A MINE! :'(\n\n");
+    }
+
     print_actual_board(board, board_height, board_width);
 
     return 0;
